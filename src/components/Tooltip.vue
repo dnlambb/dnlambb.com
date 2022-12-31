9bf2dbb387041d79da1bfb7ba9db1f410e5da409
Daniel Cordero <dn.lambb@gmail.com>
2022-12-30 23:17:25 -0300

    fix(Tooltip.vue): :bug: Change styles for correct display on hovering
    The previous Tooltip was displayed even when the mouse was not over the object. This is fixed by changing the styles.

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,61 +1,42 @@
 <template>
-  <span class="tooltip" :string-tooltip="String">
+  <div class="tooltip">
+    <span class="tooltip__text">{{stringTooltip}}</span>
     <slot></slot>
-  </span>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "Tooltip",
   props: {
-    String,
+    stringTooltip: String,
   },
 };
 </script>
 
 <style lang="scss">
 @import "../styles/colors.scss";
-
 .tooltip {
-  width: fit-content;
   position: relative;
-  font-family: 'Roboto', sans-serif;
-  font-size: calculateRem(26px);
-  color: $secondary;
-  
-  &:before,
-  &:after {
+  display: inline-block;
+  width: fit-content;
+  &__text {
+    z-index: 1;
     position: absolute;
-    content: ‘’;
-    opacity: 0;
-    transition: all 0.4s ease;
-  }
-  &:before {
-    top: -24px;
-    transform: translateY(8px);
-  }
-  &:after {
-    content: attr(string-tooltip);
-    top: -50px;
+    visibility: hidden;
+    width: fit-content;
+    bottom: 120%;
     left: 0;
-    min-width: 40px;
-    max-width: max-content;
-    height: fit-content;
-    padding: 10px;
-    font-size: calculateRem(14px);
-    font-weight: 400;
+    margin-left: 0;
+    padding: 0.375rem 1.25rem;
     text-align: center;
-    letter-spacing: calculateRem(1.6px);
-    border-radius: 8px;
-    border: 1px solid $primary;
-    background: rgba($color: #1a1c20, $alpha: 0.6);
-    backdrop-filter: blur(3px);
-    transform: translateY(8px);
+    border-radius: 4px;
+    color: $secondary;
+    background-color: $primary;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
   }
-  &:hover::before,
-  &:hover::after {
-   opacity: 1;
-   transform: translateY(-0.5px);
+  &:hover .tooltip__text {
+    visibility: visible;
   }
 }
 </style>
